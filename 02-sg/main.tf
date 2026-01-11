40139802bffefd4638f1b2c29843512c38210841
Gopichand <gummagopichand@gmail.com>
2026-01-11 09:15:26 +0530

    infra-dev

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -224,3 +224,13 @@ resource "aws_security_group_rule" "backend_default_vpc" {
   security_group_id = module.backend.sg_id
 }
 
+# Added as a part of Jenkins CICD
+resource "aws_security_group_rule" "frontend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.frontend.sg_id
+}
+
